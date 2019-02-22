1321a5fc26669c20c8fb7c922a3992b8d3cd4613
Aleksandr Serov <sashaserov2010@gmail.com>
2019-02-22 12:10:59 +0300

    Base schema

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>WebSocket App</h1>
+        <div className="content">
+          <div className="content-form">
+            <form action="#">
+              <label htmlFor="url">URL</label>
+              <input id="url" />
+
+              <label htmlFor="token">Token</label>
+              <input id="token" />
+
+              <label htmlFor="room">Room</label>
+              <input id="room" />
+
+              <button type="submit">Add</button>
+            </form>
+          </div>
+          <div className="socketList">
+            <div className="socketList-counter" />
+            <ul>
+              <li>socket 1</li>
+              <li>socket 1</li>
+              <li>socket 1</li>
+              <li>socket 1</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
