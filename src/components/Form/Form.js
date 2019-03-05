3209c087f9d3e242f1b2a5cb57cd761de027fa11
Aleksandr Serov <sashaserov2010@gmail.com>
2019-03-05 19:02:16 +0300

    Some changes

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import SocketClient from "../../services/socketClient/SocketClient";
-import "./form.css";
 import actions from "../../store/actions";
 import { connect } from "react-redux";
-import { Button, Input } from "reactstrap";
+import { Button, Input, FormGroup, Label } from "reactstrap";
+import "./form.css";
 class Form extends Component {
   state = {
-    url: "",
+    // url: "",
     token: "",
-    room: ""
+    selectedRooms: [],
+    districtId: ""
   };
   handleInput = event => {
     const { value, name } = event.target;
@@ -16,6 +17,19 @@ class Form extends Component {
       [name]: value
     });
   };
+
+  handleAddRooms = event => {
+    const { checked, name } = event.target;
+    const { selectedRooms } = this.state;
+    const update = checked
+      ? [...selectedRooms, name]
+      : selectedRooms.filter(item => item !== name);
+    console.log(update);
+    this.setState({
+      selectedRooms: update
+    });
+  };
+
   handleConnect = event => {
     event.preventDefault();
     const { token } = this.state;
@@ -32,59 +46,160 @@ class Form extends Component {
 
   handleJoinRoom = () => {
     const { dispatch, rooms } = this.props;
-    const { room } = this.state;
-    dispatch(actions.setCurrentRoom(room));
-    if (!rooms.includes(room)) {
-      dispatch(actions.updateRooms(room));
-    }
-    const districtNum = /order.district_/.test(room)
-      ? room.replace(/\D/gi, "")
-      : null;
-    switch (room) {
-      case "settings":
-        this.socket.join("settings").on({
-          room: "settings",
-          cb: msg => {
-            dispatch(actions.handleSettingsRoomsMessage(msg));
-          }
-        });
-        break;
-      case "order":
-        this.socket.join("order").on({
-          room: "order",
-          cb: msg => {
-            dispatch(actions.handleOrderRoomMessage(msg));
-          }
-        });
-        break;
-      case `order.district_${districtNum}`:
-        this.socket.join(`order.district_${districtNum}`).on({
-          room: `order.district_${districtNum}`,
-          cb: msg => {
-            dispatch(actions.handleOrdersMessage(msg));
-          }
-        });
-        break;
+    const { selectedRooms, districtId } = this.state;
+    selectedRooms.forEach(selectedRoom => {
+      selectedRoom = /order.district/.test(selectedRoom)
+        ? selectedRoom + `_${districtId}`
+        : selectedRoom;
+      if (!rooms.includes(selectedRoom)) {
+        dispatch(actions.updateRooms(selectedRoom));
+      }
 
-      default:
-        break;
-    }
+      switch (selectedRoom) {
+        case "settings":
+          this.socket.join("settings").on({
+            room: "settings",
+            cb: msg => {
+              dispatch(actions.handleSettingsRoomsMessage(msg));
+            }
+          });
+          break;
+        case "order":
+          this.socket.join("order").on({
+            room: "order",
+            cb: msg => {
+              dispatch(actions.handleOrderRoomMessage(msg));
+            }
+          });
+          break;
+        case `order.district_${districtId}`:
+          this.socket.join(`order.district_${districtId}`).on({
+            room: `order.district_${districtId}`,
+            cb: msg => {
+              dispatch(actions.handleOrdersMessage(msg));
+            }
+          });
+          break;
+
+        default:
+          break;
+      }
+    });
+    dispatch(actions.setCurrentRoom(selectedRooms[0]));
   };
 
   render() {
     return (
-      <div className="content-form">
-        <form action="#">
-          <label htmlFor="url">URL</label>
-          <Input id="url" name="url" onChange={this.handleInput} />
+      <div className="form">
+        <form action="#" className="form-form">
+          {/* <label htmlFor="url">URL</label> */}
+          {/* <Input id="url" name="url" onChange={this.handleInput} /> */}
           <label htmlFor="token">Token</label>
           <Input id="token" name="token" onChange={this.handleInput} />
           <Button onClick={this.handleConnect}>Connect</Button>
           <div>
-            <label htmlFor="room">Room</label>
-            <Input id="room" name="room" onChange={this.handleInput} />
-            <Button onClick={this.handleJoinRoom}>Join Room</Button>
+            <Label for="districtId">District ID</Label>
+            <Input
+              id="districtId"
+              name="districtId"
+              onChange={this.handleInput}
+            />
           </div>
+          <div className="form-form-checkbox">
+            <Label>Rooms</Label>
+            <FormGroup check>
+              <div className="form-form-checkbox-item">
+                <Input
+                  type="checkbox"
+                  name="logon"
+                  id="logon"
+                  onChange={this.handleAddRooms}
+                />
+                <Label for="logon" check>
+                  logon
+                </Label>
+              </div>
+
+              <div className="form-form-checkbox-item">
+                <Input
+                  type="checkbox"
+                  name="message"
+                  id="message"
+                  onChange={this.handleAddRooms}
+                />
+                <Label for="message" check onChange={this.handleInput}>
+                  message
+                </Label>
+              </div>
+              <div className="form-form-checkbox-item">
+                <Input
+                  type="checkbox"
+                  name="order"
+                  id="order"
+                  onChange={this.handleAddRooms}
+                />
+                <Label for="order" check>
+                  order
+                </Label>
+              </div>
+              <div className="form-form-checkbox-item">
+                <Input
+                  type="checkbox"
+                  name="settings"
+                  id="settings"
+                  onChange={this.handleAddRooms}
+                />
+                <Label for="settings" check>
+                  settings
+                </Label>
+              </div>
+              <div className="form-form-checkbox-item">
+                <Input
+                  type="checkbox"
+                  name="order.district"
+                  id="order.district"
+                  onChange={this.handleAddRooms}
+                />
+                <Label for="order.district" check>
+                  order.district_
+                </Label>
+              </div>
+              <div className="form-form-checkbox-item">
+                <Input
+                  type="checkbox"
+                  name="couriers.district"
+                  id="couriers.district"
+                  onChange={this.handleAddRooms}
+                />
+                <Label for="couriers.district" check>
+                  couriers.district_
+                </Label>
+              </div>
+              <div className="form-form-checkbox-item">
+                <Input
+                  type="checkbox"
+                  name="route.district"
+                  id="route.district"
+                  onChange={this.handleAddRooms}
+                />
+                <Label for="route.district" check>
+                  route.district_
+                </Label>
+              </div>
+              <div className="form-form-checkbox-item">
+                <Input
+                  type="checkbox"
+                  name="cheque.district"
+                  id="cheque.district"
+                  onChange={this.handleAddRooms}
+                />
+                <Label for="cheque.district" check>
+                  cheque.district_
+                </Label>
+              </div>
+            </FormGroup>
+          </div>
+          <Button onClick={this.handleJoinRoom}>Join Rooms</Button>
         </form>
       </div>
     );
