3ef92fcdfd997ee60237ddcef9c308de73cc13f2
Aleksandr Serov <sashaserov2010@gmail.com>
2019-03-10 13:22:34 +0300

    Fix path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import List from "./containers/List/";
-import Form from "./components/Form/";
+import Form from "./containers/Form/";
 
 class App extends Component {
   render() {
